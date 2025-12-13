a67f153b8d53ec4814e64008355daa77c6e597d2
Renanlimarj <31229314+Renanlimarj@users.noreply.github.com>
2025-12-13 12:19:14 +0000

    max connection e connection open

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows)
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version; 
+  
+  const databaseMaxConnectionsResult = await database.query("show max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  console.log(databaseVersionValue);
+
+  response.status(200).json({
+    updated_At: updatedAt,
+    dependencies: {
+      database: {
+        version : databaseVersionValue,
+        max_connections : parseInt(databaseMaxConnectionsValue)  
+      }
+    }
+  });
 }
 
 export default status;
