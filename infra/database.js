c7c3160d8518f659cceebedbcfa2efb68d6f9821
Renanlimarj <31229314+Renanlimarj@users.noreply.github.com>
2025-12-20 18:52:49 +0000

    Ajuste no database.js para ter uma função getNewClient() reutilizavel

--- infra/database.js
+++ infra/database.js
@@ -1,23 +1,11 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    ssl: true,
-  });
-  //await client.connect();
-  //const result =  await client.query (queryObject);
-  //await client.end();
-  //return result;
 
 
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -28,6 +16,23 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient () {
+    const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    ssl: true,
+  })
+
+  await client.connect()
+  return client;
+  
+}
+
+
 export default {
-  query: query,
+  query,
+  getNewClient
 };
